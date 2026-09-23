65982750766fb8f05b7a5d435778759da5e53f22
jorge2610g <scuentas150@gmail.com>
2026-09-23 17:13:29 -0300

    Agregar service worker

---
+++ sw.js
@@ -0,0 +1,10 @@
+const CACHE="meilan-v0.1.0";
+const ASSETS=["./","./index.html","./styles.css","./data.js","./app.js","./manifest.webmanifest","./icon.svg"];
+self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+self.addEventListener("fetch",e=>{
+  if(e.request.method!=="GET")return;
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{
+    const copy=res.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return res;
+  }).catch(()=>caches.match("./index.html"))));
+});
