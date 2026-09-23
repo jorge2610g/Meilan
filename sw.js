cdad5bfb30fdac6de43ddc6665cd6add542e59b5
jorge2610g <scuentas150@gmail.com>
2026-09-23 18:01:44 -0300

    Actualizar caché a v0.3.0

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="meilan-v0.2.0";
+const CACHE="meilan-v0.3.0";
 const ASSETS=["./","./index.html","./styles.css","./data.js","./app.js","./manifest.webmanifest","./icon.svg"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
